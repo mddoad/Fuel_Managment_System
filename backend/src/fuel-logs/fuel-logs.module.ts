4303c312e597a17aa59ad219e5e640682808b065
MD Doad <doadmd@gmail.com>
2026-05-10 23:20:15 +0600

    solve the fuel logs module problem

--- backend/src/fuel-logs/fuel-logs.module.ts
+++ backend/src/fuel-logs/fuel-logs.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { FuelLogsController } from './fuel-logs.controller';
 import { FuelLogsService } from './fuel-logs.service';
+import { FuelLog } from './fuel-log.entity';
+import { Station } from '../stations/station.entity';
+import { Vehicle } from '../vehicles/vehicle.entity';
+import { User } from '../users/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([FuelLog, Station, Vehicle, User])],
   controllers: [FuelLogsController],
-  providers: [FuelLogsService]
+  providers: [FuelLogsService],
 })
 export class FuelLogsModule {}
