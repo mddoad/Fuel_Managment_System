836a08814cce38a0a9f60c146a1bb0658aa3f5cd
MD Doad <doadmd@gmail.com>
2026-05-10 22:04:20 +0600

    Create dto folder in station and finish the create station and update station and wire entity int stations module

--- backend/src/stations/stations.module.ts
+++ backend/src/stations/stations.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { StationsController } from './stations.controller';
 import { StationsService } from './stations.service';
+import { Station } from './station.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Station])],
   controllers: [StationsController],
   providers: [StationsService]
 })
