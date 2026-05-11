829da4db69b6016d86b1bb6114ab25bec6799e88
MD Doad <doadmd@gmail.com>
2026-05-11 22:53:19 +0600

    create frontend folder and enale CORS in backend in main.ts and add API client to frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,10 +5,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // removes extra fields
-      forbidNonWhitelisted: true, // error if extra fields sent
+      whitelist: true,
+      forbidNonWhitelisted: true,
       transform: true,
     }),
   );
