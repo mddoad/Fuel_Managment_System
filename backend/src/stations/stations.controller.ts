09b661ebccdef93471a0acbe0f3a4f1927e9d9d5
MD Doad <doadmd@gmail.com>
2026-05-11 20:51:31 +0600

    Update station controller and vehicle controller

--- backend/src/stations/stations.controller.ts
+++ backend/src/stations/stations.controller.ts
@@ -3,12 +3,15 @@ import { AuthGuard } from '@nestjs/passport';
 import { CreateStationDto } from './dto/create-station.dto';
 import { UpdateStationDto } from './dto/update-station.dto';
 import { StationsService } from './stations.service';
-
-@UseGuards(AuthGuard('jwt'))
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+  
+@UseGuards(AuthGuard('jwt'), RolesGuard)
 @Controller('stations')
 export class StationsController {
   constructor(private readonly stationsService: StationsService) {}
 
+  @Roles('ADMIN')
   @Post()
   create(@Body() dto: CreateStationDto) {
     return this.stationsService.create(dto);
@@ -24,11 +27,13 @@ export class StationsController {
     return this.stationsService.findOne(Number(id));
   }
 
+  @Roles('ADMIN')
   @Patch(':id')
   update(@Param('id') id: string, @Body() dto: UpdateStationDto) {
     return this.stationsService.update(Number(id), dto);
   }
 
+  @Roles('ADMIN')
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.stationsService.remove(Number(id));
--- backend/src/vehicles/vehicles.controller.ts
+++ backend/src/vehicles/vehicles.controller.ts
@@ -3,12 +3,15 @@ import { AuthGuard } from '@nestjs/passport';
 import { CreateVehicleDto } from './dto/create-vehicle.dto';
 import { UpdateVehicleDto } from './dto/update-vehicle.dto';
 import { VehiclesService } from './vehicles.service';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
 
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(AuthGuard('jwt'), RolesGuard)
 @Controller('vehicles')
 export class VehiclesController {
   constructor(private readonly vehiclesService: VehiclesService) {}
-
+  
+  @Roles('ADMIN')
   @Post()
   create(@Body() dto: CreateVehicleDto) {
     return this.vehiclesService.create(dto);
@@ -24,11 +27,13 @@ export class VehiclesController {
     return this.vehiclesService.findOne(Number(id));
   }
 
+  @Roles('ADMIN')
   @Patch(':id')
   update(@Param('id') id: string, @Body() dto: UpdateVehicleDto) {
     return this.vehiclesService.update(Number(id), dto);
   }
 
+  @Roles('ADMIN')
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.vehiclesService.remove(Number(id));
